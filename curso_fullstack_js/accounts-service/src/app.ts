ced25c77205216c5cc5302e4c2d6effeef73e823
Fagundes Menezes <jlfn@gft.com>
2023-01-02 11:25:32 -0300

    Módulo 04

--- curso_fullstack_js/accounts-service/src/app.ts
+++ curso_fullstack_js/accounts-service/src/app.ts
@@ -1,11 +1,4 @@
-import express from 'express';
-import helmet from 'helmet';
 import accountsRouter from './routes/accounts';
+import app from 'ms-commons/api/app';
 
-const app = express();
-app.use(helmet());
-//Pega o corpo da requisição e transforma em json
-app.use(express.json());
-app.use(accountsRouter);
-
-export default app;
+export default app(accountsRouter);
--- curso_fullstack_js/accounts-service/src/auth.ts
+++ curso_fullstack_js/accounts-service/src/auth.ts
@@ -1,9 +1,9 @@
 import bcrypt from 'bcryptjs';
-import jwt, { VerifyOptions } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import fs from 'fs';
+import authCommons, {Token} from 'ms-commons/api/auth';
 
 const privateKey = fs.readFileSync('./keys/private.key', 'utf-8');
-const publicKey = fs.readFileSync('./keys/public.key', 'utf-8');
 const jwtExpires = parseInt(`${ process.env.JWT_EXPIRES }`);
 const jwtAlgorithm = "RS256";
 
@@ -15,8 +15,6 @@ function comparePassword(password: string, hashPassword: string){
     return bcrypt.compareSync(password, hashPassword);
 }
 
-type Token = { accountId: number }
-
 function sign(accountId: number){
     const token : Token = {accountId};
 
@@ -27,13 +25,7 @@ function sign(accountId: number){
 }
 
 async function verify(token: string){
-    try {
-        const decoded : Token = await jwt.verify(token, publicKey, { algorithms: [jwtAlgorithm] } as VerifyOptions) as Token;
-        return { accountId: decoded.accountId }
-    } catch (error) {
-        console.log(`verify: ${error}`);
-        return null;
-    }
+    return authCommons.verify(token);
 }
 
 export default { hashPassword, comparePassword, sign, verify }
